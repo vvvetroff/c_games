5d89ad618dfffdca7d84ca27023383c48fc3a428
Vladislav Vetroff <vvvetroff@Vladislavs-MacBook-Air.local>
2023-12-24 18:17:35 -0500

    made h file

---
+++ snake/snake.h
@@ -0,0 +1,16 @@
+#ifndef __SNAKE_H
+#define __SNAKE_H
+
+typedef struct Snake {
+    int** snake;
+    int  length;
+    int   headX;
+    int   headY;
+} Snake;
+
+Snake* initSnake(void);
+void printSnake(Snake* s);
+int checkGameOver(Snake* s);
+void updateSnakePositions(Snake* s);
+void freeSnake(Snake* s);
+#endif
